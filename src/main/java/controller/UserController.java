0ff3a68f532966f375238f85cfeb9e2bd4b8bb6d
Anastasia Shpachynska <shpachynska.1ki21b@gmail.com>
2023-11-10 20:37:24 +0200

    2

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import org.springframework.ui.Model;
+import entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,7 +9,15 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping("/")
-    public String register() {
+    public String register(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
         return "register";
+    }
+
+    @PostMapping("/registerUser")
+    public String registerUser(@ModelAttribute("user") User user) {
+        System.out.println(user);
+        return "home";
     }
 }
